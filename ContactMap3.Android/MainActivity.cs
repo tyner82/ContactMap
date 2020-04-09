2b67978a9e11c2d129f42ab2b597607e9bf86709
Chris Tyner <Chris.Tyner@viziya.com>
2020-04-08 21:53:55 -0400

    Update Nugets and prepare for map integration

--- ContactMap3.Android/MainActivity.cs
+++ ContactMap3.Android/MainActivity.cs
@@ -19,8 +19,9 @@ namespace ContactMap3.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.FormsMaps.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- ContactMap3.iOS/AppDelegate.cs
+++ ContactMap3.iOS/AppDelegate.cs
@@ -23,6 +23,7 @@ namespace ContactMap3.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            global::Xamarin.FormsMaps.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
